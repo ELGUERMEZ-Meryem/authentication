0940bb163af095c84c6bbc5ad804cbdd9340652c
El Guermez Meryem <elguermez.meryem@gmail.com>
2020-05-14 16:19:32 +0000

    reorganize JwtSecurity configuration

--- api/src/main/java/com/authentication/api/security/JwtSecurityConfig.java
+++ api/src/main/java/com/authentication/api/security/JwtSecurityConfig.java
@@ -4,21 +4,21 @@ import com.authentication.api.constant.SecurityConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
-@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
     private final SecurityConstants securityConstants;
 
@@ -32,21 +32,19 @@ public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManager();
     }
 
-    @Bean
-     AuthFilter authFilter() {
-        return new AuthFilter(securityConstants);
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .authorizeRequests()
+        http.cors().and()
+                .csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and()
+                .authorizeRequests()
                 .antMatchers("/api/public/**").permitAll()
                 .anyRequest().authenticated()
-        .and()
-        .addFilterBefore(authFilter(), UsernamePasswordAuthenticationFilter.class)
+                .and()
+                .addFilter(new AuthFilter(securityConstants))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         ;
     }
 
