9dc9e4fc7195944c1d4c881569ab6e18182baca6
El Guermez Meryem <elguermez.meryem@gmail.com>
2020-05-14 11:56:23 +0000

    show validation errors

--- client/src/app/auth/auth.component.ts
+++ client/src/app/auth/auth.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import * as jwt_decode from 'jwt-decode';
@@ -12,20 +12,23 @@ import {AuthService} from "./auth.service";
 })
 export class AuthComponent implements OnInit {
   isLoginMode = true;
+  isFormSubmitted = false;
   loginForm: FormGroup;
   token: string = '';
   isLoading = false;
   error: null;
   isLogin = false;
-  constructor(private http: HttpClient, private authService: AuthService) { }
+
+  constructor(private http: HttpClient, private authService: AuthService) {
+  }
 
   ngOnInit() {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('',[Validators.required, Validators.minLength(6)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
     });
     this.authService.user.subscribe(d => {
-      this.isLogin=!!d;
+      this.isLogin = !!d;
     });
     this.authService.autoLogin();
   }
@@ -37,37 +40,43 @@ export class AuthComponent implements OnInit {
   f() {
     return this.loginForm.controls;
   }
+
   onSubmit() {
-    if(this.loginForm.invalid) {
+    this.isFormSubmitted = true;
+    if (this.loginForm.invalid) {
       return;
     }
     this.isLoading = true;
-  if(this.isLoginMode) {
-    this.http.post(`http://localhost:8080/api/public/hey`, {email: this.f().email.value, password: this.f().password.value}, {responseType: 'text'}).pipe(
-      map(
-        userData => {
-          console.log("fffffffff ", userData)
-          var claims : any = jwt_decode(userData);
-          this.authService.user.next(claims.sub);
-          sessionStorage.setItem('username', claims.sub);
-          sessionStorage.setItem('expirationDate', claims.exp);
-          let tokenStr= 'Bearer '+userData;
-          sessionStorage.setItem('token', tokenStr);
-          return userData;
-        }
-      )
-    ).subscribe( data => {
-      this.token = data;
-      this.error = null;
-      this.isLoading = false;
-      this.loginForm.reset();
-    }, err => {
-      this.isLoading = false;
-      this.error = err;
-    });}
-
-    else {
-      this.http.post(`http://localhost:8080/api/public/signUp`, {email: this.f().email.value, password: this.f().password.value}, {responseType: 'text'}).subscribe( data => {
+    if (this.isLoginMode) {
+      this.http.post(`http://localhost:8080/api/public/hey`, {
+        email: this.f().email.value,
+        password: this.f().password.value
+      }, {responseType: 'text'}).pipe(
+        map(
+          userData => {
+            var claims: any = jwt_decode(userData);
+            this.authService.user.next(claims.sub);
+            sessionStorage.setItem('username', claims.sub);
+            sessionStorage.setItem('expirationDate', claims.exp);
+            let tokenStr = 'Bearer ' + userData;
+            sessionStorage.setItem('token', tokenStr);
+            return userData;
+          }
+        )
+      ).subscribe(data => {
+        this.token = data;
+        this.error = null;
+        this.isLoading = false;
+        this.loginForm.reset();
+      }, err => {
+        this.isLoading = false;
+        this.error = err;
+      });
+    } else {
+      this.http.post(`http://localhost:8080/api/public/signUp`, {
+        email: this.f().email.value,
+        password: this.f().password.value
+      }, {responseType: 'text'}).subscribe(data => {
         console.log('data ', data);
         this.token = data;
         this.isLoading = false;
@@ -85,5 +94,21 @@ export class AuthComponent implements OnInit {
     sessionStorage.removeItem('expirationDate');
     sessionStorage.removeItem('token');
     this.authService.user.next(null);
+  }
+
+  /**
+   * Checking control validation
+   *
+   * @param controlName: string => Equals to formControlName
+   * @param validationType: string => Equals to valitors name
+   */
+  isControlHasError(controlName: string, validationType: string): boolean {
+    const control = this.loginForm.controls[controlName];
+    if (!control) {
+      return false;
+    }
+
+    const result = control.hasError(validationType) && (control.dirty || control.touched || this.isFormSubmitted);
+    return result;
   }
 }
