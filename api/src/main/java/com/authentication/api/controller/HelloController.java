f34cd2983438753d9ce902918d31d102772befd3
El Guermez Meryem <elguermez.meryem@gmail.com>
2020-05-18 18:43:13 +0000

    delete tokenUtil

--- api/src/main/java/com/authentication/api/controller/HelloController.java
+++ api/src/main/java/com/authentication/api/controller/HelloController.java
@@ -1,38 +1,65 @@
 package com.authentication.api.controller;
 
+import com.authentication.api.constant.SecurityConstants;
 import com.authentication.api.entity.User;
 import com.authentication.api.repository.UserRepository;
-import com.authentication.api.security.TokenUtil;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/public")
 public class HelloController {
-
-    @Autowired
-    private TokenUtil tokenUtil;
+    private final SecurityConstants securityConstants;
 
     @Autowired
     private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public HelloController(SecurityConstants securityConstants, UserRepository userRepository) {
+        this.securityConstants = securityConstants;
+        this.userRepository = userRepository;
+    }
 
     @PostMapping("/hey")
-    public String singIn(@RequestBody User user){
+    public String singIn(@RequestBody User user) {
         final Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        String token = tokenUtil.generateToken(user);
+        String token = generateToken(user);
         return token;
     }
+
+    public String generateToken(User user) {
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(securityConstants.getSub(), user.getEmail());
+        claims.put(securityConstants.getCreate(), new Date());
+        return Jwts.builder()
+                .setClaims(claims)
+                .setExpiration(generateExpirationDate())
+                .signWith(SignatureAlgorithm.HS512, securityConstants.getTokenSecret())
+                .compact();
+    }
+
+    private Date generateExpirationDate() {
+        return new Date(System.currentTimeMillis() + securityConstants.getExpiration() * 1000);
+    }
+
     @PostMapping("/signUp")
-    public User singUp(@RequestBody User user){
+    public User singUp(@RequestBody User user) {
         user = userRepository.save(user);
         return user;
     }
--- api/src/main/java/com/authentication/api/security/AuthorizationFilter.java
+++ api/src/main/java/com/authentication/api/security/AuthorizationFilter.java
@@ -3,25 +3,23 @@ package com.authentication.api.security;
 import com.authentication.api.constant.SecurityConstants;
 import com.authentication.api.entity.User;
 import com.authentication.api.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 public class AuthorizationFilter extends OncePerRequestFilter {
     private final SecurityConstants securityConstants;
     private final UserRepository userRepository;
-
-    @Autowired
-    private TokenUtil tokenUtil;
 
     public AuthorizationFilter(SecurityConstants securityConstants, UserRepository userRepository) {
         this.securityConstants = securityConstants;
@@ -38,17 +36,50 @@ public class AuthorizationFilter extends OncePerRequestFilter {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         filterChain.doFilter(request, response);
     }
+
     private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
         final String token = request.getHeader(securityConstants.getTokenHeader());
         if (StringUtils.isNotEmpty(token) && token.startsWith(securityConstants.getTokenPrefix())) {
-            String username = tokenUtil.getUserNameFromToken(token);
+            String username = getUserNameFromToken(token);
             if (StringUtils.isNotEmpty(username)) {
                 User user = userRepository.findByEmail(username);
-                if (tokenUtil.isTokenValid(token, user)) {
+                if (isTokenValid(token, user)) {
                     return new UsernamePasswordAuthenticationToken(user, null, null);
                 }
             }
         }
         return null;
+    }
+
+    public String getUserNameFromToken(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(securityConstants.getTokenSecret())
+                    .parseClaimsJws(token.replace(securityConstants.getTokenPrefix(), ""))
+                    .getBody();
+            return (String) claims.get(securityConstants.getSub());
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public boolean isTokenValid(String token, User user) {
+        String username = getUserNameFromToken(token);
+        return ((username.equals(user.getEmail())) && !isTokenExpired(token));
+
+    }
+
+    private boolean isTokenExpired(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(securityConstants.getTokenSecret())
+                    .parseClaimsJws(token)
+                    .getBody();
+            Date expirationDate = claims.getExpiration();
+            return expirationDate.before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
