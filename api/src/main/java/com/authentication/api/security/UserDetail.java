e94ed7c54cf574b5512380313c3c2a758e4c248f
El Guermez Meryem <elguermez.meryem@gmail.com>
2020-05-19 14:29:22 +0000

    add UserDetail and UserDetailService

---
+++ api/src/main/java/com/authentication/api/security/UserDetail.java
@@ -0,0 +1,52 @@
+package com.authentication.api.security;
+
+import com.authentication.api.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetail implements UserDetails {
+    User user = new User();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+}
